e647465f29d4313154548d08e29bab7ff347f5f4
puhuajiang <puhua@umich.edu>
2020-04-17 21:51:01 -0400

    add change dataloader

--- myDataloader.py
+++ myDataloader.py
@@ -32,20 +32,36 @@ class COCODataset(data.Dataset):
         c_x = np.mean(mask.nonzero()[0])            # numpy 下的x, y坐标 与opencv相反
         c_y = np.mean(mask.nonzero()[1])
         center = torch.Tensor([c_x, c_y])
+
         ##################################################
         # TODO
         # 添加中心点周围8个点坐标及其对应边界点
         # 考虑中心点在边界的情况
         # 输入与输出的坐标变换公式为 p_in = 8p_out + 31
         # 具体分为两个阶段 第一阶段 p_in = 8p_backbone + 7 第二阶段 p_backbone = p_out + 3
+        f_cx = round(max(0, (c_x - 31) / 8))
+        f_cy = round(max(0, (c_y - 31) / 8))
+
+        valid_centers = self.get_valid_center_from_fm(25, f_cx, f_cy)
+        gt_class = self.gen_gt_class(25, valid_centers)
+
+        valid_centers_in_ori = self.coord_transform(valid_centers, 'f2o')
         ##################################################
 
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         contours = np.concatenate(contours)          # size: n * 1 * 2
         contours = contours.reshape(-1, 2)           # 此处为opencv的x, y坐标，后处理需要交换
 
-        distance, new_coord = self.get_36_coordinates(c_x, c_y, contours)  # distance is a list, new_coord is a dictionary which keys are angles(0 ~ 360, 10)
+        distances = []
+        new_coordinates = []
+        for c in valid_centers_in_ori:
+            dst, new_coord = self.get_36_coordinates(c[0], c[1], contours)  # distance is a list, new_coord is a dictionary which keys are angles(0 ~ 360, 10)
+            distances.append(dst)
+            new_coordinates.append(new_coord)
+        distances = torch.Tensor(distances)
+        new_coordinates = torch.Tensor(new_coordinates)
 
+        distance, new_coord = self.get_36_coordinates(c_x, c_y, contours)
         ####################################################
         # TODO
         # Transform image
@@ -58,7 +74,12 @@ class COCODataset(data.Dataset):
         meta['bbox'] = bbox
         meta['center'] = center
         meta['distance'] = distance
-        meta['coords'] = new_coord
+        meta['coord'] = new_coord
+        meta['targets'] = {
+            'distances': distances,
+            'gt_class': gt_class,
+        }
+        #meta['valid_centers'] = valid_centers
         # meta['mask'] = torch.Tensor(mask).resize((255, 255))
 
         return meta
@@ -121,13 +142,37 @@ class COCODataset(data.Dataset):
 
         return distances, new_coordinate
 
-annFile = './Data/instances_val2017.json'
-imgDir = './Data/val2017/'
+    def get_valid_center_from_fm(self, fm_size, c_x, c_y):
+        valid_centers = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                print(c_x + i, c_y + j)
+                if c_x + i in range(0, fm_size) and c_y + j in range(0, fm_size):
+                    valid_centers.append(np.array((c_x + i, c_y + j)))
+        return valid_centers
+
+    def gen_gt_class(self, fm_size, valid_centers):
+        gt_class = np.zeros((fm_size, fm_size))
+        for c in valid_centers:
+            gt_class[c[0], c[1]] = 1
+        return torch.Tensor(gt_class)
+
+    def coord_transform(self, coords, mode='f2o'):
+        new_coords = []
+        if mode == 'f2o':
+            for c in coords:
+                new_coords.append(8 * c + 31)
+        elif mode == 'o2f':
+            for c in coords:
+                new_coords.append(round((c - 31) / 8))
+        return new_coords
+
+
+annFile = '../../504Proj/annotations/instances_val2017.json'
+imgDir = '../../504Proj/val2017/'
 train_data = COCODataset(annFilePath=annFile, imgDir=imgDir)
-train_loader = data.DataLoader(dataset=train_data, batch_size=5, shuffle=False)
+train_loader = data.DataLoader(dataset=train_data, batch_size=1, shuffle=False)
 
-for i, Data in enumerate(train_loader):
-    if i > 0:
-        break
-    print(Data['id'])
-
+for i in train_loader:
+    print(i)
+    break
