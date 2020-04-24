a0d139ed864dd453ad2e2a069c68d789cbd84be2
WeichenZh <54957828+WeichenZh@users.noreply.github.com>
2020-04-24 16:44:47 -0400

    implemented coordinate transformation

--- davis_dataloader.py
+++ davis_dataloader.py
@@ -1,5 +1,5 @@
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps, ImageStat,ImageDraw
 import matplotlib.pyplot as plt
 import os
 import torch
@@ -199,6 +199,97 @@ class DavisDataset(data.Dataset):
                 new_coords.append(round((c - 31) / 8))
         return new_coords
 
+
+    def transform_cords(self, image, bbox, segmentation_in_original_image):
+        mean_template_and_detection = tuple(map(round, ImageStat.Stat(image).mean))
+        bbox_xywh = np.array([bbox[0] + bbox[2] // 2, bbox[1] + bbox[3] // 2, bbox[2], bbox[3]],
+                             np.float32)  # 将标注ann中的bbox从左上点的坐标+bbox长宽 -> 中心点的坐标+bbox的长宽
+        bbox_x1y1x2y2 = np.array([bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]],
+                                 np.float32)  # 将标注ann中的bbox从左上点的坐标+bbox长宽 -> 左上点的坐标+右下点的坐标
+
+        original_image_w, original_image_h = image.size
+        cx, cy, tw, th = bbox_xywh
+        p = round((tw + th) / 2, 2)
+        template_square_size = int(np.sqrt((tw + p) * (th + p)))  # a
+        detection_square_size = int(template_square_size * 2)  # A = 2a
+
+        # pad
+        detection_lt_x, detection_lt_y = cx - detection_square_size // 2, cy - detection_square_size // 2
+        detection_rb_x, detection_rb_y = cx + detection_square_size // 2, cy + detection_square_size // 2
+        left = -detection_lt_x if detection_lt_x < 0 else 0
+        top = -detection_lt_y if detection_lt_y < 0 else 0
+        right = detection_rb_x - original_image_w if detection_rb_x > original_image_w else 0
+        bottom = detection_rb_y - original_image_h if detection_rb_y > original_image_h else 0
+        padding = tuple(map(int, [left, top, right, bottom]))
+        padding_image_w, padding_image_h = left + right + original_image_w, top + bottom + original_image_h
+
+        template_img_padding = ImageOps.expand(image, border=padding, fill=mean_template_and_detection)
+        detection_img_padding = ImageOps.expand(image, border=padding, fill=mean_template_and_detection)
+
+        # crop
+        tl = cx + left - template_square_size // 2
+        tt = cy + top - template_square_size // 2
+        tr = padding_image_w - tl - template_square_size
+        tb = padding_image_h - tt - template_square_size
+        template_img_crop = ImageOps.crop(template_img_padding.copy(), (tl, tt, tr, tb))
+
+        dl = np.clip(cx + left - detection_square_size // 2, 0, padding_image_w - detection_square_size)
+        dt = np.clip(cy + top - detection_square_size // 2, 0, padding_image_h - detection_square_size)
+        dr = np.clip(padding_image_w - dl - detection_square_size, 0, padding_image_w - detection_square_size)
+        db = np.clip(padding_image_h - dt - detection_square_size, 0, padding_image_h - detection_square_size)
+        detection_img_crop = ImageOps.crop(detection_img_padding.copy(), (dl, dt, dr, db))
+
+        detection_tlcords_of_original_image = (cx - detection_square_size // 2, cy - detection_square_size // 2)
+        detection_rbcords_of_original_image = (cx + detection_square_size // 2, cy + detection_square_size // 2)
+
+        detection_tlcords_of_padding_image = (
+        cx - detection_square_size // 2 + left, cy - detection_square_size // 2 + top)
+        detection_rbcords_of_padding_image = (
+        cx + detection_square_size // 2 + left, cy + detection_square_size // 2 + top)
+
+        # resize
+        template_img_resized = template_img_crop.copy().resize((127, 127))
+        detection_img_resized = detection_img_crop.copy().resize((256, 256))
+
+        template_resized_ratio = round(127 / template_square_size, 2)
+        detection_resized_ratio = round(256 / detection_square_size, 2)
+
+        segmentation_in_padding_img = segmentation_in_original_image
+        for i in range(len(segmentation_in_original_image)):
+            for j in range(len(segmentation_in_original_image[i]) // 2):
+                segmentation_in_padding_img[i][2 * j] = segmentation_in_original_image[i][2 * j] + left
+                segmentation_in_padding_img[i][2 * j + 1] = segmentation_in_original_image[i][2 * j + 1] + top
+
+        x11, y11 = detection_tlcords_of_padding_image
+        x12, y12 = detection_rbcords_of_padding_image
+
+        segmentation_in_cropped_img = segmentation_in_padding_img
+        for i in range(len(segmentation_in_padding_img)):
+            for j in range(len(segmentation_in_padding_img[i]) // 2):
+                segmentation_in_cropped_img[i][2 * j] = segmentation_in_padding_img[i][2 * j] - x11
+                segmentation_in_cropped_img[i][2 * j + 1] = segmentation_in_padding_img[i][2 * j + 1] - y11
+                segmentation_in_cropped_img[i][2 * j] = np.clip(segmentation_in_cropped_img[i][2 * j], 0,
+                                                                x12 - x11).astype(np.float32)
+                segmentation_in_cropped_img[i][2 * j + 1] = np.clip(segmentation_in_cropped_img[i][2 * j + 1], 0,
+                                                                    y12 - y11).astype(np.float32)
+
+        segmentation_in_detection = segmentation_in_cropped_img
+        for i in range(len(segmentation_in_cropped_img)):
+            for j in range(len(segmentation_in_cropped_img[i])):
+                segmentation_in_detection[i][j] = segmentation_in_cropped_img[i][j] * detection_resized_ratio
+
+        blcords_of_bbox_in_padding_image, btcords_of_bbox_in_padding_image, brcords_of_bbox_in_padding_image, bbcords_of_bbox_in_padding_image = \
+        bbox_x1y1x2y2[0] + left, bbox_x1y1x2y2[1] + top, bbox_x1y1x2y2[2] + left, bbox_x1y1x2y2[3] + top
+        blcords_of_bbox_in_detection, btcords_of_bbox_in_detection, brcords_of_bbox_in_detection, bbcords_of_bbox_in_detection = blcords_of_bbox_in_padding_image - x11, btcords_of_bbox_in_padding_image - y11, brcords_of_bbox_in_padding_image - x11, bbcords_of_bbox_in_padding_image - y11
+        x1 = np.clip(blcords_of_bbox_in_detection, 0, x12 - x11).astype(np.float32)
+        y1 = np.clip(btcords_of_bbox_in_detection, 0, y12 - y11).astype(np.float32)
+        x2 = np.clip(brcords_of_bbox_in_detection, 0, x12 - x11).astype(np.float32)
+        y2 = np.clip(bbcords_of_bbox_in_detection, 0, y12 - y11).astype(np.float32)
+        cords_of_bbox_in_cropped_detection = np.array([x1, y1, x2, y2], dtype=np.float32)
+        cords_of_bbox_in_resized_detection = cords_of_bbox_in_cropped_detection * detection_resized_ratio
+
+        return (template_img_resized, detection_img_resized, cords_of_bbox_in_resized_detection, segmentation_in_detection)
+
     def __len__(self):
         return len(self.videos)
 
@@ -213,8 +304,26 @@ class DavisDataset(data.Dataset):
         detection_mask = (np.array(detection[1]) == mask_code).astype(np.uint8)
 
         # get bounding box from anno
-        tbbox, tc = self._get_bbox_center_from_mask(template_mask)
-        sbbox, sc = self._get_bbox_center_from_mask(detection_mask)
+        tbbox, _ = self._get_bbox_center_from_mask(template_mask)
+        sbbox, _ = self._get_bbox_center_from_mask(detection_mask)
+
+        # get segmentation and transformed segmentation
+        contours_template, _ = cv2.findContours(template_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours_detect, _ = cv2.findContours(detection_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        contours_template = np.concatenate(contours_template).reshape(-1)
+        contours_detect = np.concatenate(contours_detect).reshape(-1)
+
+        template_target, _, _, _ = self.transform_cords(template[0], tbbox, [contours_template])
+        _, detection_target, bbox_of_detection, detection_seg = self.transform_cords(detection[0], sbbox, [contours_detect])
+
+        template_target = np.array(template_target)
+        detection_target = np.array(detection_target)
+        if len(template_target.shape) == 2:
+            template_target = np.expand_dims(template_target, axis=2)
+            template_target = np.concatenate((template_target, template_target, template_target), axis=-1)
+            detection_target = np.expand_dims(detection_target, axis=2)
+            detection_target = np.concatenate((detection_target, detection_target, detection_target), axis=-1)
 
         ################################################################
         # TODO 
@@ -222,6 +331,14 @@ class DavisDataset(data.Dataset):
         # 并且需要将detection_mask变换到255坐标系下
         # template和detection 是一个list, 里面的元素分别为: 原始PIL图片， PIL格式的mask, 图片路径
         ################################################################
+        detection_mask_trans = np.zeros((detection_target.shape[0], detection_target.shape[1]), dtype=np.uint8)
+        cv2.drawContours(detection_mask_trans, [np.array(detection_seg).reshape(-1, 2)], -1, 255, cv2.FILLED)
+        kernel = np.ones((3,3), dtype=np.uint8) * 255
+        detection_mask_trans = cv2.morphologyEx(detection_mask_trans, cv2.MORPH_CLOSE, kernel)
+
+        sc_x = np.mean(detection_mask_trans.nonzero()[1])
+        sc_y = np.mean(detection_mask_trans.nonzero()[0])
+        sc = np.array([sc_x, sc_y])
 
         # find center in feature map
         f_cx = round(max(0, (sc[0] - 31) / 8))
@@ -233,7 +350,7 @@ class DavisDataset(data.Dataset):
 
         valid_centers_in_ori = self.coord_transform(valid_centers, 'f2o')
 
-        contours, _ = cv2.findContours(detection_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, _ = cv2.findContours(detection_mask_trans, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         contours = np.concatenate(contours)  # size: n * 1 * 2
         contours = contours.reshape(-1, 2)  # 此处为opencv的x, y坐标，后处理需要交换
 
@@ -257,9 +374,9 @@ class DavisDataset(data.Dataset):
             'template': template[-1],
             'detection': detection[-1],
         }
-        meta['template'] = np.array(template[0])  # 模板 3 * 127 * 127
-        meta['detection'] = np.array(detection[0])  # 检测 3 * 256 * 256
-        meta['bbox_of_detection'] = sbbox  # 检测帧坐标系下的bbox,
+        meta['template'] = template_target  # 模板 3 * 127 * 127
+        meta['detection'] = detection_target  # 检测 3 * 256 * 256
+        meta['bbox_of_detection'] = bbox_of_detection  # 检测帧坐标系下的bbox,
         meta['center'] = sc  # 检测帧坐标系下中心坐标
         meta['distance'] = distance  # 检测帧坐标系下距离
         meta['coords'] = new_coord  # 字典， 键为角度，键值为距离
@@ -268,8 +385,8 @@ class DavisDataset(data.Dataset):
             'gt_class': gt_class,  # 25 * 25
         }
         # meta['valid_centers'] = valid_centers
-        meta['mask'] = detection_mask
-
+        meta['mask'] = detection_mask_trans
+        # meta['ori_mask'] = detection_mask
         return meta
 
 
@@ -282,7 +399,6 @@ if __name__ == "__main__":
     #     fileDir = os.path.join(root_dir, cat)
     #     fileNames = os.listdir(fileDir)
     #     fileNames.sort()
-    #     print(fileNames)
 
     #     for file in fileNames:
     #         filepath = os.path.join(root_dir, cat, file)
@@ -299,10 +415,25 @@ if __name__ == "__main__":
     #         mask_cv = np.concatenate([mask_cv, mask_cv, mask_cv], axis=2)
     #         mask_cv *= 255
 
-    #         cv2.imshow("mask", mask_cv)
+    #         gray_mask = cv2.cvtColor(mask_cv, cv2.COLOR_BGR2GRAY)
+    #         contours, _ = cv2.findContours(gray_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    #         contours_ = np.concatenate(contours)  # size: n * 1 * 2
+    #         contours_ = contours_.reshape(-1)  # 此处为opencv的x, y坐标，后处理需要交换
+    #         print(contours)
+    #         print(contours_)
 
-    #         cv2.waitKey(25)
+    #         cv2.drawContours(gray_mask, contours, -1, 50, cv2.FILLED)
+    #         cv2.imshow("mask", gray_mask)
 
+    #         # contours2 = cv2.findContours(cv2.cvtColor(mask_cv, cv2.COLOR_BGR2GRAY), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    #         # print(contours2)
+    #         cv2.waitKey(0)
+
+
+
+#########################################################################
+# dataloader visualize
+########################################################################
     dataset = DavisDataset()
     loader = data.DataLoader(dataset, batch_size=1, shuffle=False)
 
@@ -326,7 +457,7 @@ if __name__ == "__main__":
         template = temp['template'][0].numpy()
         print(template.shape)
         detection = temp['detection'][0].numpy()
-        print(type(detection))
+        print((detection.shape))
         center = temp['center'][0].numpy()
         distance = temp['distance'][0].numpy()
         coords = temp['coords']
@@ -334,19 +465,7 @@ if __name__ == "__main__":
         # GT_reg = temp['targets']['distances'][0].reshape(-1, 36)
         # GT_cls = temp['targets']['gt_class'][0]
         bbox = temp['bbox_of_detection'][0]
-        GT_mask = temp['mask'][0].numpy() * 255
-        
-        # print(GT_cls.shape)
-        # pos_inds = torch.nonzero(GT_cls.view(-1) > 0).squeeze(1)
-        # print(GT_reg[pos_inds[0]])
-        # print(GT_reg[pos_inds[1]])
-        # print(GT_reg[pos_inds[2]])
-        #######################################
-        # 中心以及distance可视化
-
-        #plt.imshow(detection.permute(1, 2, 0))
-        #plt.imshow(GT_mask)
-
+        GT_mask = temp['mask'][0].numpy()
         new_coords = get_contours_from_polar(center[0], center[1], coords)
 
         mask = np.array([GT_mask, GT_mask, GT_mask]).transpose(1,2,0)
@@ -354,6 +473,13 @@ if __name__ == "__main__":
         for i in range(len(new_coords)):
             cv2.line(detection, (int(center[0]), int(center[1])), (int(new_coords[i][0]), int(new_coords[i][1])), (0,0,255), 2)
 
-        imgs = np.hstack([template, detection, mask])
+        template_show = np.zeros_like(detection)
+        template_show[64:64+127, 64:64+127, :] = template
+
+        imgs = np.hstack([template_show, detection, mask])
         cv2.imshow("imgs", imgs)
         cv2.waitKey(0)
+
+#############################################
+#
+#######################################
